5e3c88de9a4fdae9063569e2b9c9cffb55fff1e0
Kumar <futurestrading114@gmail.com>
2026-05-09 02:56:13 -0700

    update

--- api/[...path].js
+++ api/[...path].js
@@ -2,5 +2,6 @@ import app, { initializeBackend } from '../backend/server.js'
 
 export default async function handler(req, res) {
   await initializeBackend()
+  req.url = req.url.replace(/^\/api\/api(?=\/|$)/, '/api')
   return app(req, res)
 }
